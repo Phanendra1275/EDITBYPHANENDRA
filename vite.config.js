5a99044c2adef3cd5fc00224974262431a6cb5a8
NovaLoad <novaload@example.com>
2026-06-11 23:57:55 +0530

    fix: vite base path

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EDITOR---Portfolio/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  base: '/',
+  plugins: [react()],
 })
